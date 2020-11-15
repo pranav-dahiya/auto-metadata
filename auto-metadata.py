1ecb2502ec15a78c88d365cd7d5ab141ad03428a
pranav-dahiya <pranav.dahiya15@gmail.com>
2020-11-15 19:20:10 +0530

    Create auto-metadata.py

---
+++ auto-metadata.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from mutagen import flac, id3
+
+if len(sys.argv) == 2:
+      working_dir = sys.argv[1]
+else:
+      working_dir = os.getcwd()
+      
+for root, dirs, files in os.walk(working_dir):
+      tracks = [filename for filename in files if filename[-5:] == '.flac']
+      if len(tracks) > 0:
+            album_cover = flac.Picture()
+            with open(root + "/cover.jpg", 'rb') as f:
+                  album_cover.data = f.read()
+            album_cover.type = id3.PictureType.COVER_FRONT
+            album_cover.mime = u"image/jpeg"
+            artist, album = root.split("/")[-2:]
+            for filename in tracks:
+                  track_number, track_name = filename[:-5].split(" - ")
+                  track = flac.FLAC(root + "/" + filename)
+                  track['TITLE'] = track_name 
+                  track['ALBUM'] = album 
+                  track['ARTIST'] = artist 
+                  track['TRACKNUMBER'] = track_number
+                  track['TOTALTRACKS'] = str(len(tracks))
+                  track.add_picture(album_cover)
+                  track.save()
